fa61af2b40926e46a8ef08f5d14ea5f2df580774
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2022-10-21 03:09:06 -0300

    iniciando construção da função que retorna o valor do ponto de audiencia em cada praça

---
+++ assets/js/calc_functions.js
@@ -0,0 +1,70 @@
+
+
+function retornaValorPontoDeAudiencia(praca){
+
+  let mercados_array = [
+    pnt = {
+      qtd_domicilios:258.821,
+      ponto: 713.821 
+    },
+    gde_rj = {
+      qtd_domicilios:47.601,
+      ponto: 124.692, 
+    },
+    gde_campinas = {
+      qtd_domicilios:8.035,
+      ponto: 22.328, 
+    },
+    gde_bh = {
+      qtd_domicilios:20.542,
+      ponto: 55.282, 
+    },
+    gde_vitoria = {
+      qtd_domicilios:6.673,
+      ponto: 18.457, 
+    },
+    gde_porto_alegre = {
+      qtd_domicilios:16.334,
+      ponto: 40.462, 
+    },
+    gde_curitiba = {
+      qtd_domicilios:11.628,
+      ponto: 31.945, 
+    },
+    gde_florianopolis = {
+      qtd_domicilios:4.302,
+      ponto: 11.084, 
+    },
+    gde_goiania = {
+      qtd_domicilios:8.682,
+      ponto: 24.166, 
+    },
+    gde_df = {
+      qtd_domicilios:9.335,
+      ponto: 27.604, 
+    },
+    gde_salvador = {
+      qtd_domicilios:12.813,
+      ponto: 35.591, 
+    },
+    gde_fortaleza = {
+      qtd_domicilios:12.193,
+      ponto: 36.995, 
+    },
+    gde_recife = {
+      qtd_domicilios:12.529,
+      ponto: 36.186, 
+    },
+    gde_belem = {
+      qtd_domicilios:7.241,
+      ponto: 22.726, 
+    },
+    gde_manaus = {
+      qtd_domicilios:6.247,
+      ponto: 20.547, 
+    }
+  ]
+  
+
+  
+}
